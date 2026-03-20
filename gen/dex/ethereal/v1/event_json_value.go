420b2b88caf3140eec55ac3f7cb7996953036807
David <qiwi@roundinternet.money>
2026-03-20 11:23:18 -0700

    ethereal-event-values-json-compatible

--- gen/dex/ethereal/v1/event_json_value.go
+++ gen/dex/ethereal/v1/event_json_value.go
@@ -2,12 +2,12 @@ package pb
 
 var EventType_json_value = map[int32]string{
 	0: "EVENT_UNSPECIFIED",
-	1: "L2_BOOK",
-	2: "TICKER",
-	3: "TRADE_FILL",
-	4: "SUBACCOUNT_LIQUIDATION",
-	5: "POSITION_UPDATE",
-	6: "ORDER_UPDATE",
-	7: "ORDER_FILL",
-	8: "TOKEN_TRANSFER",
+	1: "L2Book",
+	2: "Ticker",
+	3: "TradeFill",
+	4: "SubaccountLiquidation",
+	5: "PositionUpdate",
+	6: "OrderUpdate",
+	7: "OrderFill",
+	8: "TokenTransfer",
 }
